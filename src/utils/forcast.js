a976cd7abfa350d7c73202728d9f6bd74f670104
Logesh Velu <logeshvelu@Logeshs-MacBook-Air.local>
2026-04-29 17:42:35 +0530

    error message updated

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -2,16 +2,17 @@ import axios from "axios";
 
 
 const forcast = async(latitude, longitude, callback) => {
-    console.log(latitude, longitude)
+    console.log('weather:::',latitude, longitude, process.env.WEATHERSTACK_KEY)
     const URL = `https://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=${latitude},${longitude}&units=f`;
     try{
-        const res = await axios.get(URL)
-        console.log('Success', res.data)
-        callback(undefined, res.data?.current?.weather_descriptions+' It is currently ' + res.data.current.temperature + ' degrees out. There is a ' + res?.data?.current?.precip + ' % chance of rain.');
-        return res.data;
+        const { data } = await axios.get(URL);
+        console.log('Success', data)
+        callback(undefined, data?.current?.weather_descriptions+' It is currently ' + data.current.temperature + ' degrees out. There is a ' + data?.current?.precip + ' % chance of rain.');
+        return data;
     }catch(error){
         if(error?.response?.data){
-            callback('Unable to find location..!', undefined);
+            console.log('test:::',)
+            callback(error?.response?.data?.error?.info || 'Unable to find location..!', undefined);
         }else{  
             callback('Unable to connect weather services!', undefined);
         }
